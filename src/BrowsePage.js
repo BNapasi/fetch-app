569ee77bbe5d1beb06478521c76b21b0e1e7b3b6
Alex Pasieka <alexspasieka@gmail.com>
2023-05-28 20:17:25 -0400

    add styling

--- src/BrowsePage.js
+++ src/BrowsePage.js
@@ -1,11 +1,13 @@
 import { useContext, useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 import { UserContext } from './UserContext';
 
 const BASE_URL = 'https://frontend-take-home-service.fetch.com'
 
 export default function BrowsePage() {
-    const { user } = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
     const [allBreeds, setAllBreeds] = useState([]);
     const [selectedBreeds, setSelectedBreeds] = useState([]);
     const [selectedCity, setSelectedCity] = useState('');
@@ -13,6 +15,20 @@ export default function BrowsePage() {
     const [selectedZipCodes, setSelectedZipCodes] = useState([]);
     const [dogIds, setdogIds] = useState([]);
     const [dogs, setDogs] = useState([]);
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    useEffect(() => {
+        getAllBreeds();
+        getDogdogIds();
+    }, []);
+
+    useEffect(() => {
+        getDogs();
+    }, [dogIds]);
+
+    useEffect(() => {
+        getDogdogIds();
+    }, [selectedZipCodes])
 
     async function getAllBreeds() {
         try {
@@ -116,51 +132,56 @@ export default function BrowsePage() {
         }
     };
 
-    useEffect(() => {
-        getAllBreeds();
-        getDogdogIds();
-    }, []);
-
-    useEffect(() => {
-        getDogs();
-    }, [dogIds]);
-
-    useEffect(() => {
-        getDogdogIds();
-    }, [selectedZipCodes])
-
     return (
-        <div>
-            <div>
-                <h1>{user.name}</h1>
-                <h1>{user.email}</h1>
-            </div>
+        <div className="grid place-content-center bg-gray-100">
+            <div className="max-w-7xl">
+                <div className="relative flex justify-between items-center py-5">
+                    <h1 className="text-3xl font-bold">FETCH</h1>
 
-            <label>Breed: </label>
-            <select onChange={(e) => setSelectedBreeds([e.target.value])}>
-                <option value="all">All</option>
-                {allBreeds.map((breed) => (
-                    <option value={breed}>{breed}</option>
-                ))}
-            </select>
+                    <button onClick={() => setShowDropdown(!showDropdown)} className="flex items-center space-x-4">
+                        <h2>{user.name}</h2>
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </button>
 
-            <label>City: </label>
-            <input onChange={(e) => setSelectedCity(e.target.value)}></input>
+                    {showDropdown && (
+                        <div className="absolute z-50 top-16 right-0 bg-white drop-shadow p-5 grid gap-4">
+                            <h1>{user.email}</h1>
+                            <button onClick={() => setUser({ name: '', email: '', authenticated: false })} className="rounded-full bg-red-500 hover:bg-red-700 text-white py-2 px-4">Logout</button>
+                        </div>
+                    )}
+                </div>
 
-            <label>State: </label>
-            <input onChange={(e) => setSelectedState(e.target.value)}></input>
+                <div className="flex pb-10">
+                    <div className="flex flex-col">
+                        <label>Breed: </label>
+                        <select onChange={(e) => setSelectedBreeds([e.target.value])}>
+                            <option value="all">All</option>
+                            {allBreeds.map((breed) => (
+                                <option value={breed}>{breed}</option>
+                            ))}
+                        </select>
 
-            <button onClick={getZipCodes}>SEARCH</button>
+                        <label>City: </label>
+                        <input onChange={(e) => setSelectedCity(e.target.value)}></input>
 
-            <div>
-                {dogs.map((dog) => (
-                    <div>
-                        <p>{dog.name}</p>
-                        <p>{dog.breed}</p>
-                        <p>{dog.zip_code}</p>
-                        <img src={dog.img} alt={dog.name} />
+                        <label>State: </label>
+                        <input onChange={(e) => setSelectedState(e.target.value)}></input>
+
+                        <button onClick={getZipCodes}>SEARCH</button>
                     </div>
-                ))}
+
+                    <div className="grid gap-4 grid-cols-5">
+                        {dogs.map((dog) => (
+                            <div className="flex flex-col justify-center items-center rounded-2xl overflow-hidden bg-white drop-shadow">
+                                <img src={dog.img} alt={dog.name} className="h-64 w-full object-cover"/>
+                                <div className="text-center my-2">
+                                    <p className="text-xl font-bold">{dog.name}</p>
+                                    <p className="text-sm">{dog.breed}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
